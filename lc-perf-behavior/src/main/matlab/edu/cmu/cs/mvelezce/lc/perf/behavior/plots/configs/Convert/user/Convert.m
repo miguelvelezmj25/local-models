d4c5513e29319a6f7a3cb0cf288588224c202dcd
Miguel Velez <miguelvelez@mijecu25.com>
2020-09-03 09:28:29 -0400

    added matlab scripts to plot perf behavior of all configurations

---
+++ lc-perf-behavior/src/main/matlab/edu/cmu/cs/mvelezce/lc/perf/behavior/plots/configs/Convert/user/Convert.m
@@ -0,0 +1,34 @@
+figure("Visible", false);
+
+train = readtable('../../../../../../../../../../../../resources/configs/java/programs/Convert/user/Convert.csv');
+times = table2array(train(:,23:23));
+times = sort(times);
+
+count = [];
+for i=1:length(times)
+    count = [count; i];
+end
+
+plot(count,times,'k','LineWidth',3);
+
+title('Density Converter', 'Fontsize',80);
+xlabel('Configurations');
+ylabel('Performance (s)');
+
+ylim([2.0, 201.0]);
+
+set(gca,'FontSize',20);
+set(gca,'xtick',[])
+
+set(gcf,'Position',[100 100 400 300])
+
+scale=2;
+paperunits='centimeters';
+filewidth=7.5;%cm
+fileheight=5.5;%cm
+filetype='pdf';
+res=300;%resolution
+size=[filewidth fileheight]*scale;
+set(gcf,'paperunits',paperunits,'paperposition',[0 0 size]);
+set(gcf, 'PaperSize', size);
+saveas(gcf,'Convert',filetype)
---
+++ lc-perf-behavior/src/main/matlab/edu/cmu/cs/mvelezce/lc/perf/behavior/plots/configs/RunBenchC/real/RunBenchC.m
@@ -0,0 +1,34 @@
+figure("Visible", false);
+
+train = readtable('../../../../../../../../../../../../resources/configs/java/programs/RunBenchC/real/RunBenchC.csv');
+times = table2array(train(:,17:17));
+times = sort(times);
+
+count = [];
+for i=1:length(times)
+    count = [count; i];
+end
+
+plot(count,times,'k','LineWidth',3);
+
+title('H2', 'Fontsize',80);
+xlabel('Configurations');
+ylabel('Performance (s)');
+
+ylim([3.0, 89.0]);
+
+set(gca,'FontSize',20);
+set(gca,'xtick',[])
+
+set(gcf,'Position',[100 100 400 300])
+
+scale=2;
+paperunits='centimeters';
+filewidth=7.5;%cm
+fileheight=5.5;%cm
+filetype='pdf';
+res=300;%resolution
+size=[filewidth fileheight]*scale;
+set(gcf,'paperunits',paperunits,'paperposition',[0 0 size]);
+set(gcf, 'PaperSize', size);
+saveas(gcf,'RunBenchC',filetype)
