b6bfaa598be60d34964b812f67c38628ffff89c1
Miguel Velez <miguelvelez@mijecu25.com>
2020-09-03 10:17:24 -0400

    wrote script to plot figures in matlab

---
+++ lc-perf-behavior/src/main/scripts/edu/cmu/cs/mvelezce/lc/perf/behavior/plots/configs/plot.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+BASE=$(pwd)
+
+cd "$BASE"/../../../../../../../../../../matlab/edu/cmu/cs/mvelezce/lc/perf/behavior/plots/configs/ || exit
+
+for d in */ ; do
+    cd "$d" || exit
+    for dc in */ ; do
+      cd "$dc" || exit
+      for m in *m; do
+        echo "Plotting" "$m"
+        rm -rf ./*.pdf
+        /Applications/MATLAB_R2019b.app/bin/matlab -nodisplay -nosplash -nodesktop -r "run('./$m'); exit;"
+      done
+      cd ..
+    done
+    cd ..
+done
