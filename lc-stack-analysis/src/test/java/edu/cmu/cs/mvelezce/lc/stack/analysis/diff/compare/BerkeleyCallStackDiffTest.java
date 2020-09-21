19290e9c86932b9c1d008a62be60f6a89ee2d560
Miguel Velez <miguelvelez@mijecu25.com>
2020-09-21 14:18:50 -0400

    diffed stacks for berkeley with sequential

--- lc-stack-analysis/src/test/java/edu/cmu/cs/mvelezce/lc/stack/analysis/diff/compare/BerkeleyCallStackDiffTest.java
+++ lc-stack-analysis/src/test/java/edu/cmu/cs/mvelezce/lc/stack/analysis/diff/compare/BerkeleyCallStackDiffTest.java
@@ -105,4 +105,102 @@ public class BerkeleyCallStackDiffTest {
             "(Ljava/nio/ByteBuffer;ZZ)V");
     differ.diff();
   }
+
+  @Test
+  public void fireProgressChanged_SEQUENTIAL_FALSE() throws IOException, DiffException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackDiff differ =
+        new CallStackDiff(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.log.LogManager",
+            "log",
+            "(Lcom/sleepycat/je/log/LogParams;)Lcom/sleepycat/je/log/LogItem;");
+    differ.diff();
+  }
+
+  @Test
+  public void addPostMarshallingInfo_SEQUENTIAL_FALSE() throws IOException, DiffException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackDiff differ =
+        new CallStackDiff(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.log.LogEntryHeader",
+            "addPostMarshallingInfo",
+            "(Ljava/nio/ByteBuffer;JLcom/sleepycat/je/utilint/VLSN;)Ljava/nio/ByteBuffer;");
+    differ.diff();
+  }
+
+  @Test
+  public void readFromFileInternal_SEQUENTIAL_FALSE() throws IOException, DiffException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackDiff differ =
+        new CallStackDiff(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.log.FileManager",
+            "readFromFileInternal",
+            "(Ljava/io/RandomAccessFile;Ljava/nio/ByteBuffer;JJ)V");
+    differ.diff();
+  }
+
+  @Test
+  public void writeToFile_SEQUENTIAL_FALSE() throws IOException, DiffException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackDiff differ =
+        new CallStackDiff(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.log.FileManager",
+            "writeToFile",
+            "(Ljava/io/RandomAccessFile;Ljava/nio/ByteBuffer;JJZ)I");
+    differ.diff();
+  }
+
+  @Test
+  public void force_SEQUENTIAL_FALSE() throws IOException, DiffException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackDiff differ =
+        new CallStackDiff(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.log.FileManager$LogEndFileDescriptor",
+            "force",
+            "()V");
+    differ.diff();
+  }
+
+  @Test
+  public void findEntry_SEQUENTIAL_FALSE() throws IOException, DiffException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackDiff differ =
+        new CallStackDiff(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.tree.IN",
+            "findEntry",
+            "([BZZLjava/util/Comparator;)I");
+    differ.diff();
+  }
+
+  @Test
+  public void serialize_SEQUENTIAL_FALSE() throws IOException, DiffException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackDiff differ =
+        new CallStackDiff(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.tree.IN",
+            "serialize",
+            "(Ljava/nio/ByteBuffer;ZZ)V");
+    differ.diff();
+  }
 }
--- lc-stack-analysis/src/test/java/edu/cmu/cs/mvelezce/lc/stack/analysis/diff/region/BerkeleyCallStackSelectorTest.java
+++ lc-stack-analysis/src/test/java/edu/cmu/cs/mvelezce/lc/stack/analysis/diff/region/BerkeleyCallStackSelectorTest.java
@@ -104,4 +104,102 @@ public class BerkeleyCallStackSelectorTest {
             "(Ljava/nio/ByteBuffer;ZZ)V");
     selector.select();
   }
+
+  @Test
+  public void log_SEQUENTIAL_FALSE() throws IOException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackSelector selector =
+        new CallStackSelector(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.log.LogManager",
+            "log",
+            "(Lcom/sleepycat/je/log/LogParams;)Lcom/sleepycat/je/log/LogItem;");
+    selector.select();
+  }
+
+  @Test
+  public void addPostMarshallingInfo_SEQUENTIAL_FALSE() throws IOException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackSelector selector =
+        new CallStackSelector(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.log.LogEntryHeader",
+            "addPostMarshallingInfo",
+            "(Ljava/nio/ByteBuffer;JLcom/sleepycat/je/utilint/VLSN;)Ljava/nio/ByteBuffer;");
+    selector.select();
+  }
+
+  @Test
+  public void readFromFileInternal_SEQUENTIAL_FALSE() throws IOException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackSelector selector =
+        new CallStackSelector(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.log.FileManager",
+            "readFromFileInternal",
+            "(Ljava/io/RandomAccessFile;Ljava/nio/ByteBuffer;JJ)V");
+    selector.select();
+  }
+
+  @Test
+  public void writeToFile_SEQUENTIAL_FALSE() throws IOException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackSelector selector =
+        new CallStackSelector(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.log.FileManager",
+            "writeToFile",
+            "(Ljava/io/RandomAccessFile;Ljava/nio/ByteBuffer;JJZ)I");
+    selector.select();
+  }
+
+  @Test
+  public void force_SEQUENTIAL_FALSE() throws IOException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackSelector selector =
+        new CallStackSelector(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.log.FileManager$LogEndFileDescriptor",
+            "force",
+            "()V");
+    selector.select();
+  }
+
+  @Test
+  public void findEntry_SEQUENTIAL_FALSE() throws IOException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackSelector selector =
+        new CallStackSelector(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.tree.IN",
+            "findEntry",
+            "([BZZLjava/util/Comparator;)I");
+    selector.select();
+  }
+
+  @Test
+  public void serialize_SEQUENTIAL_FALSE() throws IOException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    CallStackSelector selector =
+        new CallStackSelector(
+            programName,
+            "SEQUENTIAL",
+            "FALSE",
+            "com.sleepycat.je.tree.IN",
+            "serialize",
+            "(Ljava/nio/ByteBuffer;ZZ)V");
+    selector.select();
+  }
 }
