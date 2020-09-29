e486e1c4563bffa0e3696901c61e4bee9af1e74d
Miguel Velez <miguelvelez@mijecu25.com>
2020-09-29 14:50:10 -0400

    compared traces only if they are equal with two different configs

--- lc-stack-analysis/src/main/java/edu/cmu/cs/mvelezce/lc/stack/analysis/diff/compare/CallStackDiff.java
+++ lc-stack-analysis/src/main/java/edu/cmu/cs/mvelezce/lc/stack/analysis/diff/compare/CallStackDiff.java
@@ -27,42 +27,102 @@ public class CallStackDiff {
   private static final String NEW_TAG = "!";
 
   private final String programName;
-  private final int diffId;
   private final String optionValue1;
-  private final Set<String> stacks1;
   private final String optionValue2;
-  private final Set<String> stacks2;
   private final String className;
   private final String methodName;
   private final String methodSignature;
 
   public CallStackDiff(
       String programName,
-      int diffId,
       String optionValue1,
-      Set<String> stacks1,
       String optionValue2,
-      Set<String> stacks2,
       String className,
       String methodName,
       String methodSignature) {
     this.programName = programName;
-    this.diffId = diffId;
     this.optionValue1 = optionValue1;
-    this.stacks1 = stacks1;
     this.optionValue2 = optionValue2;
-    this.stacks2 = stacks2;
     this.className = className;
     this.methodName = methodName;
     this.methodSignature = methodSignature;
   }
 
   public CallStackDiff(
-      String programName, String a, String aFalse, String s, String moo, String s1) {
-    throw new UnsupportedOperationException("implement");
+      String programName,
+      int diffId,
+      String optionValue1,
+      Set<String> s1,
+      String optionValue2,
+      Set<String> s2,
+      String className,
+      String methodName,
+      String methodSignature) {
+    throw new UnsupportedOperationException("delete this constructor");
   }
 
   public void diff() throws IOException, DiffException {
+    this.clearRootDir();
+
+    System.err.println(
+        "Figure out how to compare across option values more efficiently than everything with everything");
+    Collection<File> prettyCallStacks1 = this.getPrettyCallStacks(this.optionValue1);
+    Collection<File> prettyCallStacks2 = this.getPrettyCallStacks(this.optionValue2);
+    Map<File, File> treesToDiff = this.getTreesToDiff(prettyCallStacks1, prettyCallStacks2);
+    for (Map.Entry<File, File> entry : treesToDiff.entrySet()) {
+      List<String> lines1 = FileUtils.readLines(entry.getKey(), (String) null);
+      List<String> lines2 = FileUtils.readLines(entry.getValue(), (String) null);
+      Map<String, Pair<String, String>> allMethodsToTimes = getAllMethodsToTimes(lines1, lines2);
+      List<DiffRow> diff = diffCallStacks(lines1, lines2);
+      this.generateHTML(
+          this.optionValue1,
+          entry.getKey().getName(),
+          this.optionValue2,
+          entry.getValue().getName(),
+          diff,
+          allMethodsToTimes);
+    }
+  }
+
+  private Map<File, File> getTreesToDiff(
+      Collection<File> prettyCallStacks1, Collection<File> prettyCallStacks2)
+      throws IOException, DiffException {
+    Map<File, File> treesToDiff = new HashMap<>();
+    for (File prettyCallStack1 : prettyCallStacks1) {
+      if (treesToDiff.containsKey(prettyCallStack1)) {
+        continue;
+      }
+
+      List<String> lines1 = FileUtils.readLines(prettyCallStack1, (String) null);
+      for (File prettyCallStack2 : prettyCallStacks2) {
+        if (treesToDiff.containsValue(prettyCallStack2)) {
+          continue;
+        }
+
+        List<String> lines2 = FileUtils.readLines(prettyCallStack2, (String) null);
+        if (!sameCallStackRoot(lines1, lines2)) {
+          continue;
+        }
+
+        List<DiffRow> diffs = diffCallStacks(lines1, lines2);
+        boolean equalTraces = true;
+        for (DiffRow diff : diffs) {
+          if (!diff.getTag().equals(DiffRow.Tag.EQUAL)) {
+            equalTraces = false;
+            break;
+          }
+        }
+
+        if (equalTraces) {
+          treesToDiff.put(prettyCallStack1, prettyCallStack2);
+        }
+      }
+    }
+
+    return treesToDiff;
+  }
+
+  private void clearRootDir() throws IOException {
     File outputFile =
         new File(
             OUTPUT_DIR
@@ -74,41 +134,9 @@ public class CallStackDiff {
                 + "/"
                 + this.optionValue1
                 + "-"
-                + this.optionValue2
-                + "/"
-                + this.diffId);
+                + this.optionValue2);
     if (outputFile.exists()) {
       FileUtils.cleanDirectory(outputFile);
-    }
-
-    System.err.println(
-        "Figure out how to compare across option values more efficiently than everything with everything");
-    Collection<File> prettyCallStacks1 = this.getPrettyCallStacks(this.optionValue1);
-    Collection<File> prettyCallStacks2 = this.getPrettyCallStacks(this.optionValue2);
-
-    for (File prettyCallStack1 : prettyCallStacks1) {
-      if (!this.stacks1.contains(prettyCallStack1.getName())) {
-        continue;
-      }
-      List<String> lines1 = FileUtils.readLines(prettyCallStack1, (String) null);
-      for (File prettyCallStack2 : prettyCallStacks2) {
-        if (!this.stacks2.contains(prettyCallStack2.getName())) {
-          continue;
-        }
-        List<String> lines2 = FileUtils.readLines(prettyCallStack2, (String) null);
-        if (!sameCallStackRoot(lines1, lines2)) {
-          continue;
-        }
-        Map<String, Pair<String, String>> allMethodsToTimes = getAllMethodsToTimes(lines1, lines2);
-        List<DiffRow> diff = diffCallStacks(lines1, lines2);
-        this.generateHTML(
-            this.optionValue1,
-            prettyCallStack1.getName(),
-            this.optionValue2,
-            prettyCallStack2.getName(),
-            diff,
-            allMethodsToTimes);
-      }
     }
   }
 
@@ -116,21 +144,6 @@ public class CallStackDiff {
     String root1 = lines1.get(lines1.size() - 1).split(",")[0];
     String root2 = lines2.get(lines2.size() - 1).split(",")[0];
     return root1.equals(root2);
-  }
-
-  private double getMaxSelfTime(Collection<Pair<String, String>> times) {
-    double max = 0.0;
-    for (Pair<String, String> entry : times) {
-      if (!entry.getLeft().isEmpty()) {
-        double time = Double.parseDouble(entry.getLeft());
-        max = Math.max(max, time);
-      }
-      if (!entry.getRight().isEmpty()) {
-        double time = Double.parseDouble(entry.getRight());
-        max = Math.max(max, time);
-      }
-    }
-    return max;
   }
 
   private Map<String, Pair<String, String>> getAllMethodsToTimes(
@@ -210,7 +223,7 @@ public class CallStackDiff {
       String fileName2,
       List<DiffRow> rows,
       Map<String, Pair<String, String>> allMethodsToTimes)
-      throws IOException, DiffException {
+      throws IOException {
     // Get template & replace placeholders with left & right variables with actual
     // comparison
     String template =
@@ -238,8 +251,7 @@ public class CallStackDiff {
                 + this.optionValue1
                 + "-"
                 + this.optionValue2
-                + "/"
-                + this.diffId);
+                + "/");
     if (!outputFile.exists() && !outputFile.mkdirs()) {
       throw new RuntimeException("Could not create directories");
     }
@@ -254,8 +266,7 @@ public class CallStackDiff {
       String optionValue,
       String fileName,
       boolean right,
-      Map<String, Pair<String, String>> allMethodsToTimes)
-      throws DiffException {
+      Map<String, Pair<String, String>> allMethodsToTimes) {
     StringBuilder table = new StringBuilder();
     table.append("<span style=\"font-weight:bold\">");
     table.append(fileName);
@@ -304,7 +315,7 @@ public class CallStackDiff {
     return table.toString();
   }
 
-  private static DiffRow compareTimes(Pair<String, String> times) throws DiffException {
+  private static DiffRow compareTimes(Pair<String, String> times) {
     String timeLeft = times.getLeft();
     double time1 = timeLeft.isEmpty() ? 0.0 : Double.parseDouble(timeLeft);
     String timeRight = times.getRight();
@@ -339,7 +350,7 @@ public class CallStackDiff {
     methodName = methodName.substring(0, Math.min(methodName.indexOf("("), 30));
     methodName = methodName.replaceAll("<", "[");
     methodName = methodName.replaceAll(">", "]");
-    result.append(methodName).append("(" + methodSignatureEncoded + ")");
+    result.append(methodName).append("(").append(methodSignatureEncoded).append(")");
 
     return result.toString();
   }
--- lc-stack-analysis/src/test/java/edu/cmu/cs/mvelezce/lc/stack/analysis/diff/compare/BarInfluenceCallStackDiffTest.java
+++ lc-stack-analysis/src/test/java/edu/cmu/cs/mvelezce/lc/stack/analysis/diff/compare/BarInfluenceCallStackDiffTest.java
@@ -5,34 +5,18 @@ import edu.cmu.cs.mvelezce.lc.adapters.barInfluence.BaseBarInfluenceAdapter;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
 public class BarInfluenceCallStackDiffTest {
 
   @Test
   public void bar_A_FALSE() throws IOException, DiffException {
     String programName = BaseBarInfluenceAdapter.PROGRAM_NAME;
-
     String option1 = "A";
-    Set<String> stacks1 = new HashSet<>();
-    stacks1.add("829201a0-0566-4972-9970-fe001e2b640f.csv");
-
     String option2 = "FALSE";
-    Set<String> stacks2 = new HashSet<>();
-    stacks2.add("2df0a868-c84b-4b0b-9a14-78ae068c8c62.csv");
 
     CallStackDiff differ =
         new CallStackDiff(
-            programName,
-            1,
-            option1,
-            stacks1,
-            option2,
-            stacks2,
-            "edu.cmu.cs.mvelezce.perf.BarInfluence",
-            "bar",
-            "(I)");
+            programName, option1, option2, "edu.cmu.cs.mvelezce.perf.BarInfluence", "bar", "(I)");
     differ.diff();
   }
 }
