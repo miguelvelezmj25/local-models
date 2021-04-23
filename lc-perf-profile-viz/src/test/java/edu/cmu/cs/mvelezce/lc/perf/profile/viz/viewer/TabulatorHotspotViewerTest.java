99df44dbaa1850e207e2642e4e417b21fee39256
Miguel Velez <miguelvelez@mijecu25.com>
2021-04-23 10:33:47 -0400

    got profiles for berkeley

--- lc-perf-profile-viz/src/test/java/edu/cmu/cs/mvelezce/lc/perf/profile/viz/viewer/TabulatorHotspotViewerTest.java
+++ lc-perf-profile-viz/src/test/java/edu/cmu/cs/mvelezce/lc/perf/profile/viz/viewer/TabulatorHotspotViewerTest.java
@@ -1,6 +1,7 @@
 package edu.cmu.cs.mvelezce.lc.perf.profile.viz.viewer;
 
 import edu.cmu.cs.mvelezce.adapters.convert.BaseConvertAdapter;
+import edu.cmu.cs.mvelezce.java.execute.adapters.measureDiskOrderedScan.MeasureDiskOrderedScanExecutorAdapter;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,6 +11,16 @@ public class TabulatorHotspotViewerTest {
   @Test
   public void Convert() throws IOException {
     String programName = BaseConvertAdapter.PROGRAM_NAME;
+    TabulatorHotspotViewer viewer = new TabulatorHotspotViewer(programName);
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    viewer.analyze(args);
+  }
+
+  @Test
+  public void Berkeley() throws IOException {
+    String programName = MeasureDiskOrderedScanExecutorAdapter.PROGRAM_NAME;
     TabulatorHotspotViewer viewer = new TabulatorHotspotViewer(programName);
     String[] args = new String[2];
     args[0] = "-delres";
