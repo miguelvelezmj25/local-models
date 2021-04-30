402e129a8732c99b021a72adfda7ced8f348351f
Miguel Velez <miguelvelez@mijecu25.com>
2021-04-30 13:53:50 -0400

    added profiles

--- lc-perf-profile-viz/src/test/java/edu/cmu/cs/mvelezce/lc/perf/profile/viz/viewer/TabulatorHotspotViewerTest.java
+++ lc-perf-profile-viz/src/test/java/edu/cmu/cs/mvelezce/lc/perf/profile/viz/viewer/TabulatorHotspotViewerTest.java
@@ -27,4 +27,14 @@ public class TabulatorHotspotViewerTest {
     args[1] = "-saveres";
     viewer.analyze(args);
   }
+
+  @Test
+  public void Main() throws IOException {
+    String programName = "Main";
+    TabulatorHotspotViewer viewer = new TabulatorHotspotViewer(programName);
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    viewer.analyze(args);
+  }
 }
